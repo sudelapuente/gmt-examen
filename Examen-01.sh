f6540589c3957632dfa133b7e880cf3392158083
sudelapuente <91634599+sudelapuente@users.noreply.github.com>
2021-12-12 18:22:22 -0300

    cambio a rios permanentes en leyenda

--- Examen-01.sh
+++ Examen-01.sh
@@ -134,7 +134,7 @@ clear
 	cat > tmp_leyenda <<- END
 	H 10 Times-Roman Referencias
 	N 2   
-	S 0.25c -     0.5c -     1.5p,dodgerblue2       0.75c R\355os principales
+	S 0.25c -     0.5c -     1.5p,dodgerblue2       0.75c R\355os permanentes
 	S 0.25c c 0.25c yellow   0.40p     0.5c localidades
 #	S 0.25c - 0.5c -     1p,dodgerblue2       0.75c Additional rivers
 	G 0.075c
